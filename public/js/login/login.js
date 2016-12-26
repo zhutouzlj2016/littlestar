ce2a0b683d7b2719d1beae6386ec9ef89f0cf766
点点星空丶丶 <zhutouzlj@163.com>
2016-12-26 15:03:41 +0800

    aaa

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -9,3 +9,19 @@ $(function () {
        location.href = "./index/index.html";
    },2000);
 });
+
+//$.ajax({
+//	url:'/api/user/login',
+//	type:'POST',
+//	dataType:'json',
+//	data:{
+//		username:"admin",
+//		passwd:"123456"
+//	},
+//	success:function(result){
+//		console.log('result',result)
+//	},
+//	complete:function(res){
+//		console.log('res',res)
+//	}
+//})
